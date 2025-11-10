87de9bcc3e785c5ea023d76632bccb9a78c9591b
Silvertail-UX <lelowi@tuta.io>
2025-11-10 01:39:54 +0000

    Update location.sh

--- polybar/scripts/location.sh
+++ polybar/scripts/location.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-Relay="$(mullvad status | head -n 2 | tail -n 1 | awk '{print $2}')"
 country="$(mullvad status | tail -n1 | awk '{print $3}' | tr -d ",")"
+statuss="$(mullvad status | head -n 1)"
 city="$(mullvad status | tail -n1 | awk '{print $4}' | tr -d ".")"
-
-echo -e "%{F#ffff00}    %{u-}%{F#a2fe8c}$country, $city.%{u-}"
-
+if [ "$statuss" == "Connected" ]; then
+  echo -e "%{F#ffff00}    %{u-}%{F#a2fe8c}$country - $city%{u-}"
+else [ "${statuss}" == "Disconnected" ];
+  echo -e "%{F#ffff00}    %{u-}%{F#a2fe8c}Visible Location %{F#ff0000} %{u-}"
+fi
