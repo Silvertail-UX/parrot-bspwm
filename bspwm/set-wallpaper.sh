e895e11983309af689c65f3d7d52436ddbf80cf1
silvertail-- <lelowi@tuta.io>
2026-02-25 11:13:12 -0500

    Actualizar set-wallpaper.sh

--- bspwm/set-wallpaper.sh
+++ bspwm/set-wallpaper.sh
@@ -3,15 +3,15 @@
 echo "Cambió a escritorio: $1" >> /tmp/wallpaper.log
 
 case "$1" in
-  I)   feh --bg-fill /home/lelowi/Desktop/fondos/fondo1.jpg ;;
-  II)  feh --bg-fill /home/lelowi/Desktop/fondos/fondo2.jpg ;;
-  III) feh --bg-fill /home/lelowi/Desktop/fondos/fondo3.jpg ;;
-  IV)  feh --bg-fill /home/lelowi/Desktop/fondos/fondo4.jpg ;;
-  V)   feh --bg-fill /home/lelowi/Desktop/fondos/fondo5.jpg ;;
-  VI)  feh --bg-fill /home/lelowi/Desktop/fondos/fondo6.jpg ;;
-  VII) feh --bg-fill /home/lelowi/Desktop/fondos/fondo7.jpg ;;
-  VIII) feh --bg-fill /home/lelowi/Desktop/fondos/fondo8.jpg ;;
-  IX)  feh --bg-fill /home/lelowi/Desktop/fondos/fondo9.jpg ;;
-  X)   feh --bg-fill /home/lelowi/Desktop/fondos/fondo10.jpg ;;
+  I)   feh --bg-fill $HOME/Desktop/fondos/fondo1.jpg ;;
+  II)  feh --bg-fill $HOME/Desktop/fondos/fondo2.jpg ;;
+  III) feh --bg-fill $HOME/Desktop/fondos/fondo3.jpg ;;
+  IV)  feh --bg-fill $HOME/Desktop/fondos/fondo4.jpg ;;
+  V)   feh --bg-fill $HOME/Desktop/fondos/fondo5.jpg ;;
+  VI)  feh --bg-fill $HOME/Desktop/fondos/fondo6.jpg ;;
+  VII) feh --bg-fill $HOME/Desktop/fondos/fondo7.jpg ;;
+  VIII) feh --bg-fill $HOME/Desktop/fondos/fondo8.jpg ;;
+  IX)  feh --bg-fill $HOME/Desktop/fondos/fondo9.jpg ;;
+  X)   feh --bg-fill $HOME/Desktop/fondos/fondo10.jpg ;;
 esac
 
